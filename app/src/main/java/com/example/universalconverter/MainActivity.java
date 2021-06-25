fe3b506c1a7828d18fbcade7f66b41968c48630b
Soran Alatas <soran.alatas@protonmail.com>
2021-06-25 12:54:32 +0200

    commit test 1

--- app/src/main/java/com/example/universalconverter/MainActivity.java
+++ app/src/main/java/com/example/universalconverter/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int a = 10;
+        int a = 100;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
