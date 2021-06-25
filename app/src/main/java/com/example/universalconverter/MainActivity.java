e2ab4f1909fef06fd52abd449c821486887fdc9d
Soran Alatas <soran.alatas@protonmail.com>
2021-06-25 12:52:59 +0200

    initial commit 2

---
+++ app/src/main/java/com/example/universalconverter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.universalconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int a = 10;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
