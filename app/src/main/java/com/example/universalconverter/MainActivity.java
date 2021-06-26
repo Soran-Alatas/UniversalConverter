e71a2adbf743c597d2c1c699b71868108fd6f6e8
Soran Alatas <soran.alatas@protonmail.com>
2021-06-26 22:28:08 +0200

    Added 4 buttons and access to length button

--- app/src/main/java/com/example/universalconverter/MainActivity.java
+++ app/src/main/java/com/example/universalconverter/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.universalconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+public static final String EXTRA_MESSAGE = "com.example.universalconverter.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int a = 100;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoLength(View view) {
+        Intent intent = new Intent(this, Length.class);
+        // TextView textView = (TextView) findViewById(R.id.textView);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
